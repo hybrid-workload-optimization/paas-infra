4e1d13419137cc13034c4eaf333d1527efcf38f4
hclee <hclee@sptek.co.kr>
2022-01-04 16:50:14 +0900

    인벤토리 관련 클래스 생성

---
+++ src/main/java/kr/co/sptek/paas/service/InventoryService.java
@@ -0,0 +1,14 @@
+package kr.co.sptek.paas.service;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.sptek.paas.model.Cluster;
+
+@Service
+public class InventoryService {
+
+	public String genInventory(Cluster cluster) { 
+		return "";
+	}
+	
+}
